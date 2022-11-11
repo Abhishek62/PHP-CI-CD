703f6d8f06e06dae4fba87634909a233a39358be
Abhishek62 <70894731+Abhishek62@users.noreply.github.com>
2022-11-11 10:35:01 +0530

    Add files via upload

---
+++ index.php
@@ -0,0 +1,281 @@
+<!DOCTYPE html>
+<html>
+<head>
+<link rel="shortcut icon" href="images/favicon.ico">
+<meta charset="utf-8">
+<meta http-equiv="X-UA-Compatible" content="IE=edge">
+<meta name="viewport" content="width=device-width, initial-scale=1">
+<meta name="description" content="">
+<meta name="author" content="">
+<title>Bluedot</title>
+<link href="css/bootstrap.min.css" rel="stylesheet">
+<link rel="stylesheet" href="css/all.css">
+<link rel="stylesheet" href="css/animate.css">
+<link rel="stylesheet" href="css/owl.carousel.min.css">
+<link rel="stylesheet" href="css/owl.theme.default.min.css">
+<link href="css/style.css" rel="stylesheet">
+<link href="css/device.css" rel="stylesheet">
+<link href="css/theme.css" rel="stylesheet">
+<meta name="robots" content="noindex">
+</head>
+<body>
+<header>
+  <div class="container">
+    <div class="row">
+      <div class="col-lg-12"> <a href="index.html" class="logo wow fadeInDown" data-wow-duration="1.5s" data-wow-delay="0ms"><img src="images/logo.png" alt=""/></a> </div>
+    </div>
+  </div>
+</header>
+<section class="banner">
+  <div class="bg-banner"><img src="images/banner.png" alt=""/></div>
+  <div class="container">
+    <div class="row">
+      <div class="col-lg-12">
+        <div class="banner-content">
+          <h1 class="wow fadeInRight" data-wow-duration="1.5s" data-wow-delay="50ms">Ayurvedic Treatments</h1>
+          <h2 class="wow zoomInRight" data-wow-duration="1.5s" data-wow-delay="200ms">Treat your body as <br>
+            a whole <span>traditionally</span></h2>
+          <a href="javascript:void(0)" class="quotebtn wow fadeInUp" data-wow-duration="1.5s" data-wow-delay="400ms" id="contactbtn">GET A FREE QUOTE</a> </div>
+      </div>
+    </div>
+  </div>
+</section>
+<section class="inner-section sympsection">
+  <div class="container">
+    <div class="row">
+      <div class="col-lg-12 text-center wow fadeIn" data-wow-duration="1.5s" data-wow-delay="50ms">
+        <h1>Cure the root problem, not just the symptoms</h1>
+        <div class="centcover">
+          <p>Ever considered getting an Ayurvedic treatment for your well-being? Ayurveda has no side-effects! It is the most effective, safest, cost-effective treatment of all.</p>
+          <p>We, at <span class="bluebgt">Bluedot</span> can help you reach the destination you desire for your ayurvedic treatment, anywhere in the world. Select a treatment that is emotionally, culturally and economically viable for you and we will handle all your other concerns. Depending on individuals, we offer customizable packages based on the need and treatment opted. Our experts will guide and support you throughout, to help choose the best package for you; in doing so, and more, we give you the best that medical assistance has to offer.</p>
+        </div>
+      </div>
+    </div>
+  </div>
+</section>
+<section class="inner-section treatbg">
+  <div class="container">
+    <div class="row">
+      <div class="col-lg-12">
+        <h1>LIST OF TREATMENTS OFFERED</h1>
+        <p>Because it is holistic, ayurveda is successful at healing illnesses</p>
+      </div>
+      <div class="col-lg-12">
+        <div class="managesection">
+          <div class="mgsecone wow fadeInLeft" data-wow-duration="1.5s" data-wow-delay="50ms">
+            <h2>WEIGHT <br>
+              MANAGEMENT</h2>
+            <h3>Weight management refers to a set of practices and behaviors that are necessary to keep one's weight at a healthful level.</h3>
+          </div>
+          <div class="mgsectwo"><img src="images/treatbg1.png" alt=""/></div>
+          <div class="mgsecthree">
+            <h4><span>Ayurvedic benefits :</span> <br>
+              Ayurvedic treatments help promote healthy weight loss/ gain in a safe and effective way. Panchakarma, for example, is a scientifically proven ancient detoxifying technique that is wisely tailored to cleanse your body.</h4>
+          </div>
+        </div>
+        <div class="managesection">
+          <div class="mgsecone wow fadeInRight" data-wow-duration="1.5s" data-wow-delay="50ms">
+            <h2>ARTHRITIS</h2>
+            <h3>Arthritis is an inflammation of the joints. It can affect one joint or multiple joints.There are more than 100 different types of arthritis, with different causes and treatment methods.</h3>
+          </div>
+          <div class="mgsectwo"><img src="images/treatbg2.png" alt=""/></div>
+          <div class="mgsecthree">
+            <h4><span>Ayurvedic benefits :</span> <br>
+              To cure the chronic, recurring inflammation in the joints, ayurvedic treatments focus on stabilising the vata disturbance that leads to pain. Customized therapies are suggested considering the severity of the disease.</h4>
+          </div>
+        </div>
+        <div class="managesection">
+          <div class="mgsecone wow fadeInLeft" data-wow-duration="1.5s" data-wow-delay="50ms">
+            <h2>DIABETES</h2>
+            <h3>Diabetes is a chronic, metabolic disease characterized by elevated levels of blood glucose which leads over time to serious damage to the heart, blood vessels, eyes, kidneys and nerves.</h3>
+          </div>
+          <div class="mgsectwo"><img src="images/treatbg3.png" alt=""/></div>
+          <div class="mgsecthree">
+            <h4><span>Ayurvedic benefits :</span> <br>
+              Ayurvedic solution for type-1 and type-2 Diabetes addresses the root-cause of the problem and gives you 360° improvement, beyond just lowered blood sugar levels. Treatment for the advanced stage, works on improving the overall quality of life through customized Rasayanic treatment, diets and simple lifestyle modifications. </h4>
+          </div>
+        </div>
+        <div class="managesection">
+          <div class="mgsecone wow fadeInRight" data-wow-duration="1.5s" data-wow-delay="50ms">
+            <h2>REJUVENATION <br>
+              THERAPY</h2>
+            <h3>The legendary Rasayana Chikitsa, or Rejuvenation Therapy, is often considered synonymous with rebirth, for it completely revitalizes the body, eliminating the ill-effects of all previous disorders.</h3>
+          </div>
+          <div class="mgsectwo"><img src="images/treatbg4.png" alt=""/></div>
+          <div class="mgsecthree">
+            <h4><span>Ayurvedic benefits :</span> <br>
+              Rejuvenation programs enhances the body’s functions and promotes youthfulness, lightness, better digestion, and higher energy level. Rasayana Chikitsa is a holistic revitalization therapy aimed at treating nutrient deficiencies, with a focus on the constituent elements of the body. </h4>
+          </div>
+        </div>
+      </div>
+      <div class="col-lg-12 text-center"> <a href="#" class="morebt">and more..</a> </div>
+    </div>
+  </div>
+</section>
+<section class="secbanner">
+  <div class="secbgban"><img src="images/secbg.png" alt=""/></div>
+  <div class="container-fluid">
+    <div class="row">
+      <div class="col-lg-12">
+        <h2>Making your Ayurveda treatment, at the best centre, possible for you.</h2>
+        <h3>The future is here.</h3>
+      </div>
+    </div>
+  </div>
+</section>
+<section class="certisection">
+  <div class="container">
+    <div class="row gx-2">
+      <div class="col-xl-3 col-lg-6">
+        <div class="sapbox wow fadeInRight" data-wow-duration="1.5s" data-wow-delay="50ms"> <span><img src="images/ic1.png" alt=""/></span>
+          <h2>CERTIFIED <br>
+            CENTERS</h2>
+          <h3>Certified authentic and traditional Ayurvedic hospitals and centers</h3>
+        </div>
+      </div>
+      <div class="col-xl-3 col-lg-6">
+        <div class="sapbox wow fadeInLeft" data-wow-duration="1.5s" data-wow-delay="50ms"> <span><img src="images/ic2.png" alt=""/></span>
+          <h2>STAR <br>
+            ACCOMMODATION</h2>
+          <h3>Only 4 to 7 Star hospitality partners are hand-picked</h3>
+        </div>
+      </div>
+      <div class="col-xl-3 col-lg-6">
+        <div class="sapbox wow fadeInRight" data-wow-duration="1.5s" data-wow-delay="50ms"> <span><img src="images/ic3.png" alt=""/></span>
+          <h2>ANYTIME, <br>
+            ANYWHERE</h2>
+          <h3>We're always on call to assist you globally</h3>
+        </div>
+      </div>
+      <div class="col-xl-3 col-lg-6">
+        <div class="sapbox wow fadeInLeft" data-wow-duration="1.5s" data-wow-delay="50ms"> <span><img src="images/ic4.png" alt=""/></span>
+          <h2>SPECIAL <br>
+            NEEDS</h2>
+          <h3>Clients with special needs receive extra care</h3>
+        </div>
+      </div>
+    </div>
+  </div>
+</section>
+<section class="offersection">
+  <div class="container-fluid">
+    <div class="row">
+      <div class="col-lg-12">
+        <h1>WHAT MORE DO WE OFFER?</h1>
+        <h2>Bluedot is known for setting higher-than-industry standards in the medical travel and medical assistance sector with these additional services:</h2>
+      </div>
+    </div>
+  </div>
+  <div class="container">
+    <div class="row">
+      <div class="col-xl-6">
+        <div class="offcondec"> <span class="wow zoomIn" data-wow-duration="2s" data-wow-delay="20ms"><img src="images/ic5.png" alt=""/></span>
+          <h3>Medical Visas and<br>
+         Documentation</h3>
+        </div>
+      </div>
+      <div class="col-xl-6">
+        <div class="offcondec"> <span class="wow zoomIn" data-wow-duration="2s" data-wow-delay="20ms"><img src="images/icon-35-1-(4) (1).png" alt=""/></span>
+          <h3>Travel Assistance,<br>
+           Pick and Drop Services</h3>
+        </div>
+      </div>
+      <div class="col-xl-6">
+        <div class="offcondec"> <span class="wow zoomIn" data-wow-duration="2s" data-wow-delay="20ms"><img src="images/ic7.png" alt=""/></span>
+          <h3>Medical Travel with<br>
+           Leisure Packages</h3>
+        </div>
+      </div>
+      <div class="col-xl-6">
+        <div class="offcondec"> <span class="wow zoomIn" data-wow-duration="2s" data-wow-delay="20ms"><img src="images/ic8.png" alt=""/></span>
+          <h3>24-Hrs Dedicated Case <br>
+            Managers</h3>
+        </div>
+      </div>
+    </div>
+  </div>
+</section>
+<section class="whsectapwap">
+  <div class="container">
+    <div class="row">
+      <div class="col-xl-12">
+        <div class="whocontent wow zoomIn" data-wow-duration="1.5s" data-wow-delay="50ms">
+          <div class="tacklepack">
+            <h2>WHO ARE WE?</h2>
+            <p>Headquartered in Dubai, Bluedot Medical Assistance is Middle East’s most sought after provider of Medical Assistance Services and has a presence globally; serving in Asia, Africa , GCC, Europe and CIS countries as well. We provide comprehensive and highly personalized care, which is the core of our medical assistance services, to individuals/groups who travel for quality and cost-effective treatment options across the globe. Our team works around the clock to ensure that every client gets personalised service as per their need.</p>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+</section>
+<section class="contabtg" id="contact">
+  <div class="container-fluid">
+    <div class="row">
+      <div class="col-xl-12">
+        <div class="contact-cont">
+          <h1>Get your Ayurveda treatment facilitated through us</h1>
+          <p>Connect with us by filling your information below. You get details on exclusive packages and treatment plans along with an in-house support team to guide you through questions about choosing flights, planning your journey, appointments, billing and more.</p>
+          
+          <form name="frmContact" id="frmContact" action="../contact.php">
+          <input type="hidden" name="subject" value="Ayurveda Treatment Enquiry">
+          
+          <?php include '../contact_form.html';?>
+
+          </form>
+
+          
+
+        </div>
+      </div>
+    </div>
+  </div>
+</section>
+<footer>
+  <div class="container">
+    <div class="row">
+      <div class="col-xl-5">
+        <div class="footerlogo"><img src="images/footer-logo.png" alt=""/></div>
+      </div>
+      <div class="col-xl-4">
+        <h2>UAE</h2>
+        <p>Office No: 6, 4th Floor<br>
+          Al Naboodah Al Shoala Building,<br>
+          Port Saeed, Deira, P O Box: 31923<br>
+          Dubai, UAE</p>
+        <h2>INDIA</h2>
+        <p>S101, 4th Floor, Cresens Tower,<br>
+          Pathadipalam, Kochi, Kerala,<br>
+          India, PIN -682033</p>
+      </div>
+      <div class="col-xl-3">
+        <div class="phsecpack">
+          <div class="embgcpck">
+            <h3>Phone</h3>
+            <h4>+971 55 188 1441 <br>
+              +91 974 541 0000</h4>
+            <h3>Email</h3>
+            <h4>mail@bluedotassist.com</h4>
+            <h3>Network</h3>
+            <ul>
+              <li><a href="#"><i class="fa-brands fa-facebook-f"></i></a></li>
+              <li><a href="#"><i class="fa-brands fa-twitter"></i></a></li>
+              <li><a href="#"><i class="fa-brands fa-linkedin-in"></i></a></li>
+              <li><a href="#"><i class="fa-brands fa-instagram"></i></a></li>
+            </ul>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+</footer>
+<script src="js/jquery-3.6.0.min.js"></script> 
+<script src="js/popper.min.js"></script> 
+<script src="js/bootstrap.min.js"></script> 
+<script src="js/owl.carousel.min.js"></script> 
+<script src="js/custom.js"></script> 
+<script src="js/wow.js"></script> 
+<script>new WOW().init();</script>
+</body>
+</html>
+<script src="../mail.js"></script>
